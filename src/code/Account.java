64bb8c70f42a8a11b097703b9088722e28e79fe9
JalenMcC <76828457+JalenMcC@users.noreply.github.com>
2021-11-11 22:38:53 -0500

    Moved a comment section

--- src/code/Account.java
+++ src/code/Account.java
@@ -1,5 +1,35 @@
 package code;
 
+
+
 public class Account {
 
 }
+
+
+/*
+ * Could create an account object for easy access and storage
+ * 		Could also add other values later as the account
+ * 		grows to hold different things
+ * 
+ * public class Account
+ * 
+ * - private AccountKey variable
+ * 		(dont know the type yet)
+ * - private String username
+ * - private String password
+ * - private String accountName
+ * 
+ * - default constructor
+ * - parameterized constructor 
+ * 
+ * - getAccountKey
+ * - getAccountName
+ * - getAccountUsername
+ * - getAccountPassword
+ * 
+ * - setAccountName
+ * - setAccountUsername
+ * - setAccountPassword
+ * 
+ */
--- src/code/Main.java
+++ src/code/Main.java
@@ -64,32 +64,6 @@ public class Main {
 		 * 
 		 */
 		
-		/*
-		 * Could create an account object for easy access and storage
-		 * 		Could also add other values later as the account
-		 * 		grows to hold different things
-		 * 
-		 * public class Account
-		 * 
-		 * - private AccountKey variable
-		 * 		(dont know the type yet)
-		 * - private String username
-		 * - private String password
-		 * - private String accountName
-		 * 
-		 * - default constructor
-		 * - parameterized constructor 
-		 * 
-		 * - getAccountKey
-		 * - getAccountName
-		 * - getAccountUsername
-		 * - getAccountPassword
-		 * 
-		 * - setAccountName
-		 * - setAccountUsername
-		 * - setAccountPassword
-		 * 
-		 */
 		
 		/*
 		 * Other things to add later
